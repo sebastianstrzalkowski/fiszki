bd27348d549a4943501c10d724d732a663210bf3
sebastianstrzalkowski <33623243+sebastianstrzalkowski@users.noreply.github.com>
2018-02-25 13:31:29 +0100

    Update Config.java

--- src/pl/strzalkowskisebastian/datebase/Config.java
+++ src/pl/strzalkowskisebastian/datebase/Config.java
@@ -1,9 +1,9 @@
 package pl.strzalkowskisebastian.datebase;
 
 public class Config {
-    protected String dbHost = "80.211.241.41";
-    protected String dbPort = "3306";
-    protected  String dbUser = "seba";
-    protected String dbPass = "haslo";
-    protected String dbName = "Fiszki";
+    protected String dbHost = "";
+    protected String dbPort = "";
+    protected  String dbUser = "";
+    protected String dbPass = "";
+    protected String dbName = "";
 }
